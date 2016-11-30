a5ab48103d7b810ee179922cecbb464981818b21
Renato Mangini <mangini@google.com>
2016-11-29 19:46:27 -0800

    Fix javadoc and add missing licensing.
    Change-Id: I0b1b174b443c34d9106760ed341c581f8dd1c143

--- app/src/main/java/com/example/androidthings/myproject/MainActivity.java
+++ app/src/main/java/com/example/androidthings/myproject/MainActivity.java
@@ -21,10 +21,22 @@ import android.os.Bundle;
 import android.util.Log;
 
 /**
- * Skeleton of the main Android IoT activity. Implement your device's logic in this class.
+ * Skeleton of the main Android Things activity. Implement your device's logic
+ * in this class.
  *
- * The recommended way of handling I/O is by starting a new thread. Avoid handling blocking
- * operations in the main thread.
+ * Android Things peripheral APIs are accessible through the class
+ * PeripheralManagerService. For example, the snippet below will open a GPIO pin and
+ * set it to HIGH:
+ *
+ * <pre>{@code
+ * PeripheralManagerService service = new PeripheralManagerService();
+ * mLedGpio = service.openGpio("BCM6");
+ * mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+ * mLedGpio.setValue(true);
+ * }</pre>
+ *
+ * For more complex peripherals, look for an existing user-space driver, or implement one if none
+ * is available.
  *
  */
 public class MainActivity extends Activity {
